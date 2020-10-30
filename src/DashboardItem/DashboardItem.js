bc3f812175258d8c5000a677cf4c9b0961f71ad6
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-30 10:07:25 -0500

    created fuction for check in change, and created saga

--- src/DashboardItem/DashboardItem.js
+++ src/DashboardItem/DashboardItem.js
@@ -1,31 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class DashboardItem extends Component {
+	state = {
+		isCheckedIn: this.props.pet.isCheckedIn,
+	};
 
-  componentDidMount = () => {
-    this.getPets();
-  }
+	componentDidMount = () => {
+		this.getPets();
+	};
 
-  getPets = () => {
-    this.props.dispatch({
-        type: 'FETCH_PETS'
-    })
-  }
+	getPets = () => {
+		this.props.dispatch({
+			type: "FETCH_PETS",
+		});
+	};
 
-  render() {
-    // console.log('this.props for dashboarditem.js:', this.props)
-    return (
-      <tr>
-        <td>{this.props.pet.ownerName}</td>
-        <td>{this.props.pet.petName}</td>
-        <td>{this.props.pet.breed}</td>
-        <td>{this.props.pet.color}</td>
-        <td>{this.props.pet.isCheckedIn.toString()}</td>
-        <td><button>Delete</button><button>Check</button></td>
-      </tr>
-    );
-  }
+	heckOut = () => {
+		this.setState({
+			isCheckedIn: !this.state.isCheckedIn,
+		});
+		let objectToSend = {
+			isCheckedIn: this.state.isCheckedIn,
+			id: this.props.pet.id,
+		};
+		this.props.dispatch({
+			type: "UPDATE_STATUS",
+			payload: objectToSend,
+		});
+	};
+
+	render() {
+		console.log("this is the state", this.state);
+
+		return (
+			<tr>
+				<td>{this.props.pet.ownerName}</td>
+				<td>{this.props.pet.petName}</td>
+				<td>{this.props.pet.breed}</td>
+				<td>{this.props.pet.color}</td>
+				<td>{this.props.pet.isCheckedIn.toString()}</td>
+				<td>
+					<button>Delete</button>
+					{this.state.isCheckedIn ? (
+						<button onClick={this.heckOut}>Check Out</button>
+					) : (
+						<button onClick={this.heckOut}>Check</button>
+					)}
+				</td>
+			</tr>
+		);
+	}
 }
 
 export default connect()(DashboardItem);
--- src/index.js
+++ src/index.js
@@ -1,76 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import createSagaMiddleware from 'redux-saga';
-import axios from 'axios';
-import { put, takeEvery } from 'redux-saga/effects';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import createSagaMiddleware from "redux-saga";
+import axios from "axios";
+import { put, takeEvery } from "redux-saga/effects";
 
 const petReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_PETS':
-      return action.payload;
-    default:
-      return state;
-  };
+	switch (action.type) {
+		case "SET_PETS":
+			return action.payload;
+		default:
+			return state;
+	}
 };
 
 const ownersReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_OWNERS':
-        return action.payload;
-      default:
-        return state;
-    };
-  };
+	switch (action.type) {
+		case "SET_OWNERS":
+			return action.payload;
+		default:
+			return state;
+	}
+};
 
-function* fetchPetsSaga(){
-    console.log( 'in fetchPetsSaga' )
-    let response = yield axios({
-        method: 'GET',
-        url: '/pets'
-    })
-    console.log( 'response is:', response.data )
+function* fetchPetsSaga() {
+	console.log("in fetchPetsSaga");
+	let response = yield axios({
+		method: "GET",
+		url: "/pets",
+	});
+	console.log("response is:", response.data);
 
-    yield put({type: 'SET_PETS', payload: response.data})
+	yield put({ type: "SET_PETS", payload: response.data });
 }
 
-function* fetchOwnersSaga(){
-    console.log( 'in fetchOwnersSaga' )
-    let response = yield axios({
-        method: 'GET',
-        url: '/owners'
-    })
-    console.log( 'response is:', response.data )
-    yield put({type: 'SET_OWNERS', payload: response.data})
+function* fetchOwnersSaga() {
+	console.log("in fetchOwnersSaga");
+	let response = yield axios({
+		method: "GET",
+		url: "/owners",
+	});
+	console.log("response is:", response.data);
+	yield put({ type: "SET_OWNERS", payload: response.data });
 }
 
+function* updateStatusSaga(action) {
+	console.log("in updateStatusSaga", "this is action.payload:", action.payload);
+	let isCheckedIn = action.payload.isCheckedIn;
+	let id = action.payload.id;
+	let response = yield axios({
+		method: "PUT",
+		url: `/pets/${id}`,
+		data: action.payload.isCheckedIn,
+	});
+	console.log("response is:", action.payload);
+	console.log("ischeckedin is:", isCheckedIn);
+	// yield put({ type: "SET_OWNERS", payload: response.data });
+}
 
-// Generator function to listen for specific strings to run sagas. 
-function* sagaListener (){
-    yield takeEvery( 'FETCH_PETS', fetchPetsSaga );
-    yield takeEvery( 'FETCH_OWNERS', fetchOwnersSaga )
+// Generator function to listen for specific strings to run sagas.
+function* sagaListener() {
+	yield takeEvery("FETCH_PETS", fetchPetsSaga);
+	yield takeEvery("FETCH_OWNERS", fetchOwnersSaga);
+	yield takeEvery("UPDATE_STATUS", updateStatusSaga);
 }
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  combineReducers({ 
-    petReducer, 
-    ownersReducer
-  }),
-  applyMiddleware(sagaMiddleware)
+	combineReducers({
+		petReducer,
+		ownersReducer,
+	}),
+	applyMiddleware(sagaMiddleware)
 );
 // Run sagaMiddleware
 sagaMiddleware.run(sagaListener);
 
-
-ReactDOM.render(<Provider store={store}> <App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		{" "}
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
